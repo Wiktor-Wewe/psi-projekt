129bf336ed4123b36eec1b8f3bfab7421f260a2b
Wiktor-wewe <wewersajtys.wiktor@gmail.com>
2023-11-16 10:05:49 +0100

    szkielet aplikacji, modele, trochę walidacji, kontrolery i kilka metod, baza inmemory / sqlite, jwt

---
+++ LibraryAPI/LibraryAPI/Controllers/AuthorsController.cs
@@ -0,0 +1,39 @@
+﻿using LibraryAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Controllers
+{
+    [Route("api/Authors")]
+    [ApiController]
+    public class AuthorsController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public AuthorsController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAuthors() 
+        {
+            return Ok(_dbContext.Authors.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult CreateAuthor(CreateAuthorDto author) 
+        {
+            var newAuthor = new Author()
+            {
+                Name = author.Name,
+                Surname = author.Surname
+            };
+
+            _dbContext.Authors.Add(newAuthor);
+            _dbContext.SaveChanges();
+
+            return Ok(_dbContext.Authors.FirstOrDefault(a => a.Name == author.Name && a.Surname == author.Surname));
+        }
+
+    }
+}
---
+++ LibraryAPI/LibraryAPI/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace LibraryAPI.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string? JobPosition { get; set; }
+    } 
+
+    public class CreateEmployeeDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string? JobPosition { get; set; }
+    }
+}
